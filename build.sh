44b9787dc7981dd455e0ad4ce3b692a331c8b32a
ShellTux <115948079+ShellTux@users.noreply.github.com>
2023-06-08 23:36:24 +0100

    Update scripts

--- build.sh
+++ build.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 
-sudo rm --recursive --force ./output
-mkdir --verbose --parents ./pre-build
+cp -r ./releng ./pre-build
+notify-send "Edit pre build script"
 $EDITOR ./pre-build.sh
 ./pre-build.sh
 
-sudo mkarchiso -v -w ./output -o ./output releng
-sudo chown root:"$(whoami)"
-sudo chmod g+w ./output
+sudo mkarchiso -v -w ./output -o ./output ./pre-build
+notify-send --urgency=critical "Ended building ISO"
+sudo chown --recursive root:"$(whoami)"
+sudo chmod --recursive g+w ./output
 
 find ./output -name "*.iso" -exec mv --verbose {} . \;
-# sudo rm --recursive --force ./output
-find . -name "*.iso" | head -1 | xargs run_archiso -i
+sudo rm --recursive --force ./output
+rmdir ./pre-build
+printf 'Open ISO in QEMU [s/N]: '
+read -r confirm
+[ "$confirm" = "s" ] || [ "$confirm" = "S" ] \
+	&& find . -name "*.iso" | head -1 | xargs run_archiso -i
