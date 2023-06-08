92d4c07593889416c3b7e805b488c5e6a50ee7b5
ShellTux <115948079+ShellTux@users.noreply.github.com>
2023-06-08 05:08:53 +0100

    call pre-build.sh

--- build.sh
+++ build.sh
@@ -3,10 +3,12 @@
 sudo rm --recursive --force ./output
 mkdir --verbose --parents ./pre-build
 $EDITOR ./pre-build.sh
+./pre-build.sh
 
-sudo mkdir --verbose --parents ./output
-sudo chown root:"$(whoami)"
 sudo mkarchiso -v -w ./output -o ./output releng
+sudo chown root:"$(whoami)"
+sudo chmod g+w ./output
 
-mv --verbose ./output/*.iso .
+find ./output -name "*.iso" -exec mv --verbose {} . \;
+# sudo rm --recursive --force ./output
 find . -name "*.iso" | head -1 | xargs run_archiso -i
